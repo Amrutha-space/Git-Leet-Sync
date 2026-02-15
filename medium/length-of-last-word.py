1d5fd82904cce613d1834543aa6d82bf242181e4
Amrutha-space <annepuamrutha98@gmail.com>
2026-02-15 16:20:58 +0530

    Add solution for Length of Last Word

---
+++ medium/length-of-last-word.py
@@ -0,0 +1,19 @@
+/*
+ * LeetCode Solution: Length of Last Word
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/length-of-last-word/
+ * Date: 15/02/2026, 16:20:22
+ * Solution: Initial
+ */
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i, length = len(s) - 1, 0
+
+        while s[i] == " ":
+            i -= 1
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+        return length
